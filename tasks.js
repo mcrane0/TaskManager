86a1325d1deb36b3c540676422116678f55df271
Miles Crane <miles.crane@stu.barren.kyschools.us>
2023-02-28 10:55:42 -0600

    completed task manager project

--- tasks.js
+++ tasks.js
@@ -25,16 +25,58 @@ let num; // For storing the number of the task to be removed
 
 let removed; // For storing the value of the removed task
 
+
 // Displays the menu for the user to select an option. Also, sets the user's response to the userInput variable.
 // ???
 let userInput = prompt(menu);
-
 
 // Loops and continues to the menu until the user ends/closes the Task Manager 
 //  (AKA Until the user enters CLOSE (???))
 while (userInput.toUpperCase() !== "CLOSE"){
 
-    break; // break can be commented out when userInput = CLOSE is set up
+    // display tasks to user
+    if (userInput.toUpperCase() === "TASKS"){
+        showTasks = "";
+        for (t = 0; t < tasks.length; t++){
+            showTasks += (tasks[t] + "\n");
+        }
+        alert(showTasks);
+    }
+    // add a task
+    else if (userInput.toUpperCase() === "NEW"){
+        newTask = "";
+        while (newTask === ""){ // error handling: while nothing is entered as the new task
+            newTask = prompt("Enter the new task:");
+        }
+        tasks.push(newTask);
+        alert("\"" + newTask + "\" has been added.");
+    }
+    // remove a task
+    else if (userInput.toUpperCase() === "REMOVE"){
+        showTasks = "";
+        for (r = 0; r < tasks.length; r++){
+            showTasks += ((r + 1) + ": " + tasks[r] + "\n");
+        }
+
+        num = parseInt( prompt("Enter the number for a task to remove:\n" + showTasks) );
+        while ( !( (num >= 1) && (num <= tasks.length) ) ){ // error handling: while the number inputted is NOT from 1 to the length of the array
+            alert("Not a valid option.");
+            num = prompt("Enter the number for a task to remove:\n" + showTasks);
+        }
+        num--;
+
+        removed = tasks.splice(num, 1);
+        alert("\"" + removed[0] + "\" has been removed.");
+    }
+    // user enters something that isn't an option
+    else {
+        alert("Not a valid option.");
+    }
+
+    userInput = prompt(menu); // Displays the menu again
+
+    // break;   break can be commented out when userInput = CLOSE is set up
 }
+
 
 alert(`Thank you for using the Task Manager`); // Alerts the user that they have closed the program
